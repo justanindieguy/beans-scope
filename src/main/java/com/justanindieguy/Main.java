7a05ac05195d23e6397abbe7238f16c4fab67f2b
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-16 14:47:44 -0600

    retrieve Person Bean from context in main method

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -2,12 +2,15 @@ package com.justanindieguy;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.justanindieguy.beans.Person;
 import com.justanindieguy.config.ProjectConfig;
 
 public class Main {
 
   public static void main(String[] args) {
     var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+    Person person = context.getBean(Person.class);
+    System.out.println("Person bean from Context: " + person);
   }
 
 }

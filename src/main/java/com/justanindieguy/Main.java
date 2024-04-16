2af906dfd7eb7235eddcc6334beb14b4a728653e
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-16 16:57:58 -0600

    check dependencies between Beans, display the bean names for Person and Vehicle type

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -1,23 +1,30 @@
 package com.justanindieguy;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.justanindieguy.beans.Person;
 import com.justanindieguy.beans.Vehicle;
-import com.justanindieguy.beans.VehicleServices;
 import com.justanindieguy.config.ProjectConfig;
+import com.justanindieguy.services.VehicleServices;
 
 public class Main {
 
   public static void main(String[] args) {
     var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+    String[] persons = context.getBeanNamesForType(Person.class);
+    String[] vehicles = context.getBeanNamesForType(Vehicle.class);
+    System.out.println(Arrays.toString(persons));
+    System.out.println(Arrays.toString(vehicles));
+
     Person person = context.getBean(Person.class);
     System.out.println("Person bean from Context: " + person);
 
     Vehicle vehicle = person.getVehicle();
     VehicleServices vehicleServices = vehicle.getVehicleServices();
-    vehicleServices.getSpeakers().makeSound();
-    vehicleServices.getTyres().rotate();
+    vehicleServices.playMusic();
+    vehicleServices.moveVehicle();
   }
 
 }

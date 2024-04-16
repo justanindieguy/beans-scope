32577a30d75a3e831b18bb5cd427f1cb0a45cc7a
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-16 14:41:09 -0600

    start Spring Context in main method

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -1,7 +1,13 @@
 package com.justanindieguy;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.justanindieguy.config.ProjectConfig;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+  public static void main(String[] args) {
+    var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+  }
+
 }

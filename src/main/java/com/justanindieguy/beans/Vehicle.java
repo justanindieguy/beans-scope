a8881b77f5b1a229cc27b7db0ac0bbd215c3ec6b
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-16 14:49:58 -0600

    create Vehicle Bean

---
+++ src/main/java/com/justanindieguy/beans/Vehicle.java
@@ -0,0 +1,30 @@
+package com.justanindieguy.beans;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class Vehicle {
+
+  private String name;
+
+  @PostConstruct
+  public void initialize() {
+    name = "Honda";
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Vehicle [name=" + name + "]";
+  }
+
+}

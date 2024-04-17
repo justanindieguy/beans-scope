8cc8bb1e67981333f749990edcf69332a7f3bf19
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-17 12:49:00 -0600

    learn how eager instantiation of Singleton Beans works

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -1,30 +1,17 @@
 package com.justanindieguy;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.justanindieguy.beans.Person;
-import com.justanindieguy.beans.Vehicle;
 import com.justanindieguy.config.ProjectConfig;
-import com.justanindieguy.services.VehicleServices;
 
 public class Main {
 
   public static void main(String[] args) {
     var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-    String[] persons = context.getBeanNamesForType(Person.class);
-    String[] vehicles = context.getBeanNamesForType(Vehicle.class);
-    System.out.println(Arrays.toString(persons));
-    System.out.println(Arrays.toString(vehicles));
-
-    Person person = context.getBean(Person.class);
-    System.out.println("Person bean from Context: " + person);
-
-    Vehicle vehicle = person.getVehicle();
-    VehicleServices vehicleServices = vehicle.getVehicleServices();
-    vehicleServices.playMusic();
-    vehicleServices.moveVehicle();
+    System.out.println("Before retrieving the Person Bean from the Spring Context");
+    Person person = context.getBean(Person.class); // <- Person Bean is created before trying to access it.
+    System.out.println("After retrieving the Person Bean from the Spring Context");
   }
 
 }
